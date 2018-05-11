c1a80b09a8f2912e94a18c8384509ffabdf2ad37
savior0319 <savior0319@naver.com>
2018-05-11 18:14:51 +0900

    jquery

---
+++ HTML/HTML_prac/jQuery/WebContent/test.js
@@ -0,0 +1,9 @@
+
+	window.onload = function(){
+		document.getElementById("btn1").onclick = function(){
+			alert("Hello1");
+		}
+        document.getElementById("btn2").onclick = function(){
+			alert("Hello2");
+		}
+	}
